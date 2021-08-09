58b441c3a40d04db1924bb808bc8c04314187648
Ashwini S <Ashwini_S3@Dellteam.com>
2021-08-09 16:19:37 +0530

    Unit testing project

---
+++ BookStore.ProductService/TestProject/Services/ProductTests.cs
@@ -0,0 +1,32 @@
+﻿using BookStore.ProductService.Controllers;
+using BookStore.ProductService.Models;
+using BookStore.ProductService.Persistence;
+using BookStore.UnitTests.Fake;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace BookStore.UnitTests.Services
+{
+    public class ProductTests
+    {
+        public void Get_Returns_ActionResults()
+        {
+            // Arrange
+            var mockRepo = new Mock<IProductRepository>();
+            mockRepo.Setup(repo => repo.GetAll()).Returns(new ProductData().GetProductList());
+            var controller = new ProductController(mockRepo.Object);
+
+            // Act
+            var result = controller.Get();
+
+            // Assert
+            var viewResult = Assert.IsType<OkObjectResult>(result);
+            var model = Assert.IsAssignableFrom<IEnumerable<Product>>(viewResult.Value);
+            Assert.NotNull(model);
+            Assert.Equal(2, model.Count());
+        }
+    }
+}
